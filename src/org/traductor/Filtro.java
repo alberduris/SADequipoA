ff645bdf9f34b443918f588131353da34ca6e647
SarkV <sarktec@gmail.com>
2016-03-11 10:18:08 +0100

    filtro

--- src/org/traductor/Filtro.java
+++ src/org/traductor/Filtro.java
@@ -1,8 +1,33 @@
 package org.traductor;
 
+import java.io.File;
+import java.io.IOException;
+
+import weka.core.Instances;
+import weka.core.converters.CSVLoader;
+
 public class Filtro {
 	
+	 
 	public Filtro(){
 		
+	}
+	
+	public void filtrar(String path){
+		 // load CSV 
+	    CSVLoader loader = new CSVLoader();
+	    try {
+			loader.setSource(new File(path));
+			//loader.
+		    
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] arg){
+		Filtro f = new Filtro();
+		f.filtrar("tweetSentiment.train.csv");
 	}
 }
