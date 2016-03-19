b8bf2764ee9c91c7f20f2eea374300c2d9f16406
Alber <4lb3rduris@gmail.com>
2016-03-19 14:34:45 +0100

    BATCH FILTERING
    Conseguido para los 3 archivos [Train,Test,Blind] !!!

--- src/org/sad/labo4/Preprocess.java
+++ src/org/sad/labo4/Preprocess.java
@@ -15,21 +15,22 @@ public class Preprocess {
 	static StringToWordVector stwv;
 	static InfoGainAttributeEval infoGain;
 
-	public static void stringToWordVector(Instances pDatos) {
+	public static void stringToWordVector() {
 
 		stwv = new StringToWordVector();
 
 		try {
-			stwv.setInputFormat(pDatos);
+			stwv.setInputFormat(DataHolder.getDatosTrain());
 			stwv.setAttributeIndices("3");
 			stwv.setWordsToKeep(99999);
-			stwv.setLowerCaseTokens(true);// Aumenta algo el porcentaje pero
-											// ¿Ad-Hoc?
-			stwv.setStemmer(new LovinsStemmer());// Aumenta algo el porcentaje
-													// --> Stemmer buena idea
-			stwv.setIDFTransform(true);// ¿No hace nada?
-			Instances nuevosDatos = Filter.useFilter(pDatos, stwv);
-			DataHolder.setDatosTrain(nuevosDatos);
+			stwv.setLowerCaseTokens(true);// Aumenta algo el porcentaje pero ¿Ad-Hoc? 
+			stwv.setStemmer(new LovinsStemmer());// Aumenta algo el porcentaje Stemmer buena idea 
+			stwv.setIDFTransform(true);// ¿No hace nada? 
+			
+			//Guardar todos los nuevos datos con batch filtering
+			DataHolder.setDatosTrain(Filter.useFilter(DataHolder.getDatosTrain(), stwv));
+			DataHolder.setDatosTest(Filter.useFilter(DataHolder.getDatosTest(), stwv));
+			DataHolder.setDatosBlind(Filter.useFilter(DataHolder.getDatosBlind(), stwv));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -38,35 +39,27 @@ public class Preprocess {
 
 	public static void infoGain() {
 
-		try{
-		    weka.filters.supervised.attribute.AttributeSelection filter = new weka.filters.supervised.attribute.AttributeSelection();
+		try {
+			weka.filters.supervised.attribute.AttributeSelection filter = new weka.filters.supervised.attribute.AttributeSelection();
 
-		    InfoGainAttributeEval infoGain = new InfoGainAttributeEval();
-		    Ranker search = new Ranker();
-		    search.setThreshold(0);
-		    
-		    filter.setEvaluator(infoGain);
-		    filter.setSearch(search);
-		    filter.setInputFormat(DataHolder.getDatosTrain());
-		    Instances newData = Filter.useFilter(DataHolder.getDatosTrain(), filter);
-		    DataHolder.setDatosTrain(newData);
-		}
-		catch(Exception e){
+			InfoGainAttributeEval infoGain = new InfoGainAttributeEval();
+			Ranker search = new Ranker();
+			search.setThreshold(0);
+
+			filter.setEvaluator(infoGain);
+			filter.setSearch(search);
+			filter.setInputFormat(DataHolder.getDatosTrain());
+			
+			//Guardar todos los nuevos datos con batch filtering
+			DataHolder.setDatosTrain(Filter.useFilter(DataHolder.getDatosTrain(), filter));
+			DataHolder.setDatosTest(Filter.useFilter(DataHolder.getDatosTest(), filter));
+			DataHolder.setDatosBlind(Filter.useFilter(DataHolder.getDatosBlind(), filter));
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
-	}
-
-	public static void selectAttributes() {
-		
-		
-	}
-
-	public static void applyFilter() {
-
-		// TODO
-		// Filter.useFilter(filter, DataHolder.getDatos());
 
 	}
+
+	
 
 }
--- src/org/sad/labo4/Principal.java
+++ src/org/sad/labo4/Principal.java
@@ -1,7 +1,11 @@
 package org.sad.labo4;
 
+import java.util.Scanner;
+
 public class Principal {
 
+	static Scanner scanner = new Scanner(System.in);
+	
 	//La ejecucion del main
 	// java -jar nombreJar.jar train.arff test.arff blind.arff
 	
@@ -10,16 +14,20 @@ public class Principal {
 		
 		//Cargar datos
 		DataHolder.loadTrainData(args[0]);
-		//DataHolder.loadTestData(args[1]);
-		//DataHolder.loadBlindData(args[2]);
+		DataHolder.loadTestData(args[1]);
+		DataHolder.loadBlindData(args[2]);
 				
 		//Preprocesado
-		Preprocess.stringToWordVector(DataHolder.getDatosTrain());
-		Preprocess.selectAttributes();
+		Preprocess.stringToWordVector();
+		Preprocess.infoGain();
 		
 		
 		//Imprimir
 		DataHolder.printAttributos(DataHolder.getDatosTrain());
+		scanner.nextLine();
+		DataHolder.printAttributos(DataHolder.getDatosTest());
+		scanner.nextLine();
+		DataHolder.printAttributos(DataHolder.getDatosBlind());
 
 	}
 
